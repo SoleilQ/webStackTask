a1eec28f0e16c4e4ce731adae96fbe972c82cf6c
SoleilQ <416100854@qq.com>
2019-03-24 21:03:04 +0800

    task

--- yii图书管理系统/basic/config/db.php
+++ yii图书管理系统/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=library',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
